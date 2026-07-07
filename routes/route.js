93c03c758ca39f292fbd9f45d149ed530a8732d5
Osondu Gabriel <136438699+OsonduGabriel@users.noreply.github.com>
2026-07-07 12:54:17 +0100

    Added route to get quiz by id - GET/resources/:id

--- routes/route.js
+++ routes/route.js
@@ -17,7 +17,21 @@ route.get('/', async (req, res) => {
 
 //GET quizzes using a particular ID from the client
 route.get('/:id', async (req, res) => {
+    const quizId = req.params.id
+    try {
+        const question = await getQuizById(quizId)
 
+        if(question){
+            res.status(200).json(question)
+            return;
+        }
+
+        res.status(404).json({message: `Quiz with id:${quizId} not found`})
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: "Internal server error"})
+    }
 })
 
 export default route
