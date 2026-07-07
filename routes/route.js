f425f58b2ade745757d40be57955441c9342bdbd
Osondu Gabriel <136438699+OsonduGabriel@users.noreply.github.com>
2026-07-07 14:49:47 +0100

    Added DELETE /resources/:id endpoint to delete quiz from file storage  using id

--- routes/route.js
+++ routes/route.js
@@ -67,4 +67,22 @@ route.put('/:id', async (req, res) => {
 
 })
 
+// DELETE - delete a quiz from data/questions.json using the quiz id
+route.delete("/:id", async (req, res) => {
+    const quizId = req.params.id
+
+    try {
+        const deletedQuiz = await deleteQuiz(quizId)
+
+        if(deletedQuiz){
+            return res.status(200).json({message: "Quiz successfully deleted"})
+        }
+
+        return res.status(404).json({message: `Quiz with id: ${quizId} not found` })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: "Internal Server Error"})
+    }
+})
+
 export default route
